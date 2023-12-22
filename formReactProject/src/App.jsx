9965310ebe5299c1fba2706a558cf8a8f9c00dda
psandesh64 <psandesh64@gmail.com>
2023-12-22 16:02:06 +0545

    Second Commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -4,15 +4,19 @@ import viteLogo from '/vite.svg'
 // import './App.css'
 
 const App = () => {
-  const [persons, setPersons] = useState([{name:'Arto Hellas'}])
+  const [persons, setPersons] = useState([{name:'Arto Hellas',id:0}])
   const [newName, setNewName] = useState('')
   
   const handlePerson = (event) =>
   {
     event.preventDefault()
-    const copy=[...persons]
-    copy[persons.length]={name:newName}
-    setPersons(copy)
+    // const copy=[...persons]
+    // copy[persons.length]={name:newName}
+
+    // const copy=[...persons,{name:newName}]
+
+    setPersons(persons.concat({name:newName,id:persons[persons.length-1].id+1}))
+    console.log(persons)
     setNewName('')
   }
   const handleNameChange = (event) =>
@@ -25,18 +29,16 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handlePerson}>
         <div>
-          name : <input 
+          Name : <input 
           value={newName}
           onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type='submit'>add</button>
+          <button type='submit'>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>)
-      )}
+        <p key={person.id}>{person.name}</p>)
+      )} 
     </div>
   )
 
