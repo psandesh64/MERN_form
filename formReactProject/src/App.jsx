d5bf5fed8333bcf52e1681e12ff3e45fae93e9ce
psandesh64 <psandesh64@gmail.com>
2023-12-26 07:18:22 +0545

    first commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -6,6 +6,7 @@ import viteLogo from '/vite.svg'
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas', id: 0 }, { name: 'Arto Hell', id: 1 }])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState(0)
 
   const handlePerson = (event) => {
     event.preventDefault()
@@ -17,15 +18,18 @@ const App = () => {
       alert('Name already exists in the phonebook!');
     } else {
       // If not present, add the new person to the list
-      setPersons(persons.concat({ name: newName, id: persons[persons.length - 1].id + 1 }))
+      setPersons(persons.concat({ name: newName,number:newNumber, id: persons[persons.length - 1].id + 1 }))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
-
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
@@ -35,12 +39,16 @@ const App = () => {
             value={newName}
             onChange={handleNameChange}
           />
+          Number : <input
+            value={newNumber}
+          onChange={handleNumberChange}
+        />
           <button type='submit'>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.id}>{person.name}</p>)
+        <p key={person.id}>{person.name} {person.number}</p>)
       )}
     </div>
   )
