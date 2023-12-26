82d2e8be457ec284821bc8452b202c41ac61c634
psandesh64 <psandesh64@gmail.com>
2023-12-26 14:09:10 +0545

    second commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -4,55 +4,71 @@ import viteLogo from '/vite.svg'
 // import './App.css'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', id: 0 }, { name: 'Arto Hell', id: 1 }])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '9841388801', id: 0 },
+    { name: 'Grivans Maddox', number: '9832658596', id: 1 }
+  ]);
+
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchPhonebook = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   const handlePerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    // Check if the new name is already present
     const isNamePresent = persons.some((person) => person.name === newName);
 
     if (isNamePresent) {
       alert('Name already exists in the phonebook!');
     } else {
-      // If not present, add the new person to the list
-      setPersons(persons.concat({ name: newName,number:newNumber, id: persons[persons.length - 1].id + 1 }))
-      setNewName('')
-      setNewNumber('')
+      setPersons([
+        ...persons,
+        { name: newName, number: newNumber, id: persons.length }
+      ]);
+      setNewName('');
+      setNewNumber('');
     }
-  }
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
+
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter shown with: <input onChange={searchPhonebook} value={searchTerm} />
+
       <form onSubmit={handlePerson}>
         <div>
-          Name : <input
-            value={newName}
-            onChange={handleNameChange}
-          />
-          Number : <input
-            value={newNumber}
-          onChange={handleNumberChange}
-        />
+          Name :{' '}
+          <input value={newName} onChange={handleNameChange} />
+          Number :{' '}
+          <input value={newNumber} onChange={handleNumberChange} />
           <button type='submit'>Add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.id}>{person.name} {person.number}</p>)
-      )}
+      {filteredPersons.map((person) => (
+        <p key={person.id}>
+          {person.name} {person.number}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
