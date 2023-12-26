084494a80243dbd370728defd04c3c924e82db21
psandesh64 <psandesh64@gmail.com>
2023-12-26 12:36:42 +0545

    third commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
-import axios from axios
+import axios from 'axios'
 
 const App = () => {
+  const [persons,setPersons] = useState([])
 
-  const promise = axios.get('http://localhost:3001/notes')
-  promise.then(response => {
-    console.log(response)
-  })
+  useEffect(()=>{
+  const promise = axios.get('http://localhost:3001/phonebook')
+    promise.then(response => {
+      setPersons(response.data)
+    })
+  },[])
+ 
   return (
     <div>
       <h2>Numbers</h2>
-      {persons.map((response) => (
-        <p key={response.data.id}>{response.data.name}</p>)
+      {persons.map((data) => (
+        <p key={data.id}>{data.id}  {data.name} :: {data.number}</p>)
       )}
     </div>
   )
