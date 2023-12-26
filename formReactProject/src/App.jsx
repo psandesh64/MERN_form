0532a18a4b2218691a1a034f7b96f9090f94d516
psandesh64 <psandesh64@gmail.com>
2023-12-26 06:39:42 +0545

    first commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -4,29 +4,34 @@ import viteLogo from '/vite.svg'
 // import './App.css'
 
 const App = () => {
-  const [persons, setPersons] = useState([{name:'Arto Hellas',id:0}])
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', id: 0 }, { name: 'Arto Hell', id: 1 }])
   const [newName, setNewName] = useState('')
-  
-  const handlePerson = (event) =>  {
+
+  const handlePerson = (event) => {
     event.preventDefault()
-    
-    const check=persons.forEach((person) => {
-    person.name === newName ? 
-    alert("Already contact Present"):
-    setPersons(persons.concat({name:newName,id:persons[persons.length-1].id+1}))
+
+    // Check if the new name is already present
+    const isNamePresent = persons.some((person) => person.name === newName);
+
+    if (isNamePresent) {
+      alert('Name already exists in the phonebook!');
+    } else {
+      // If not present, add the new person to the list
+      setPersons(persons.concat({ name: newName, id: persons[persons.length - 1].id + 1 }))
+      setNewName('')
+    }
   }
-    )
-    setNewName('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
   }
-  const handleNameChange = (event) =>  {
-      setNewName(event.target.value)
-  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handlePerson}>
         <div>
-          Name :  <input
+          Name : <input
             value={newName}
             onChange={handleNameChange}
           />
@@ -36,10 +41,10 @@ const App = () => {
       <h2>Numbers</h2>
       {persons.map((person) => (
         <p key={person.id}>{person.name}</p>)
-      )} 
+      )}
     </div>
   )
-
 }
+
 
 export default App
