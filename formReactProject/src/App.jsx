27952cee26844656d20023e10bb55fd63ed85095
psandesh64 <psandesh64@gmail.com>
2023-12-26 11:30:06 +0545

    second commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import axios from 'axios'
 // import './App.css'
+import axios from axios
 
 const App = () => {
-  const  promise = axios.get('http://localhost:3001/notes')
-  console.log(promise)
-  return <h1>axios</h1>
+
+  const promise = axios.get('http://localhost:3001/notes')
+  promise.then(response => {
+    console.log(response)
+  })
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {persons.map((response) => (
+        <p key={response.data.id}>{response.data.name}</p>)
+      )}
+    </div>
+  )
 }
 
 
