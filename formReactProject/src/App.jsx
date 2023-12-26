874ca18b7f3df1e82235d4f80dc7be960878b416
psandesh64 <psandesh64@gmail.com>
2023-12-26 08:33:46 +0545

    first commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -1,57 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 // import './App.css'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', id: 0 }, { name: 'Arto Hell', id: 1 }])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState(0)
-
-  const handlePerson = (event) => {
-    event.preventDefault()
-
-    // Check if the new name is already present
-    const isNamePresent = persons.some((person) => person.name === newName);
-
-    if (isNamePresent) {
-      alert('Name already exists in the phonebook!');
-    } else {
-      // If not present, add the new person to the list
-      setPersons(persons.concat({ name: newName,number:newNumber, id: persons[persons.length - 1].id + 1 }))
-      setNewName('')
-      setNewNumber('')
-    }
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={handlePerson}>
-        <div>
-          Name : <input
-            value={newName}
-            onChange={handleNameChange}
-          />
-          Number : <input
-            value={newNumber}
-          onChange={handleNumberChange}
-        />
-          <button type='submit'>Add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.id}>{person.name} {person.number}</p>)
-      )}
-    </div>
-  )
+  const  promise = axios.get('http://localhost:3001/notes')
+  console.log(promise)
+  return <h1>axios</h1>
 }
 
 
