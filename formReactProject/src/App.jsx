404211c2394a9c2cf98a7ff1b01e6087c5fd4869
psandesh64 <psandesh64@gmail.com>
2023-12-31 21:12:11 +0545

    first commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -3,9 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import axios from 'axios'
+import Person from './components/phonebook'
+
 
 const App = () => {
   const [persons,setPersons] = useState([])
+  const [formArray,setformArray]=useState({'name':'','number':''})
 
   useEffect(()=>{
   const promise = axios.get('http://localhost:3001/phonebook')
@@ -13,9 +16,29 @@ const App = () => {
       setPersons(response.data)
     })
   },[])
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    const phonebookObj = {
+      id : persons.length + 1,
+      name :formArray.name,
+      number:formArray.number
+    }
+    axios.post("http://localhost:3001/phonebook",phonebookObj)
+    .then(response=>setPersons(persons.concat(response.data)))
+    setformArray({'name':'','number':''})
+  }
  
   return (
     <div>
+      <form onSubmit={handleChange}>
+      <label>Name : </label>
+      <input onChange={(event)=>setformArray({...formArray,'name':event.target.value})} value={formArray.name}/><br/><br/>
+      <label>Number : </label>
+      <input onChange={(event)=>setformArray({...formArray,'number':event.target.value})} value={formArray.number}/>
+      <button>Add</button>
+      <br/><br/>
+    </form>
       <h2>Numbers</h2>
       {persons.map((data) => (
         <p key={data.id}>{data.id}  {data.name} :: {data.number}</p>)
---
+++ formReactProject/src/components/phonebook.jsx
@@ -0,0 +1,7 @@
+const Person =({person}) =>{
+    return <div>
+        <p>{person.name}</p>
+        <p>{person.number}</p>
+    </div>
+}
+export default Person 
