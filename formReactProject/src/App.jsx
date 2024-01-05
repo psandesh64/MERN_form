43571cfaca27279e96ab8b5c19e87e034c0eed7a
psandesh64 <psandesh64@gmail.com>
2024-01-05 20:17:44 +0545

    first commit

--- formReactProject/src/App.jsx
+++ formReactProject/src/App.jsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 import Person from './components/person'
 import phonebook_ser from './services/phonebook'
+import NotificationMsg from './components/notificationmsg'
 
 
 const App = () => {
   const [persons,setPersons] = useState([])
   const [formArray,setformArray]=useState({'name':'','number':''})
+  const [notification,setNotification] = useState({status:'',css:'success'})
+  
 
   useEffect(()=>{
     phonebook_ser.getAll().then(response => {
       setPersons(response.data)
     })
+    .catch(error=>console.log('error fetching file'))
   },[])
   const handleChange = (e) => {
     e.preventDefault();
@@ -37,25 +41,36 @@ const App = () => {
   
     if (!foundPerson) {
       phonebook_ser.create(phonebookObj)
-        .then(response => setPersons(persons.concat(response.data)));
+        .then(response => setPersons(persons.concat(response.data)))
+        .catch(error=>console.log('error creating file'));
+        setNotification({status:'Created Successfully',css:'success'});
+        setTimeout(()=>setNotification(''),2000);
     } else {
       phonebook_ser.update(newId, phonebookObj)
-        .then(response => setPersons(persons.map(person => (person.id === newId ? response.data : person))));
+        .then(response => setPersons(persons.map(person => (person.id === newId ? response.data : person))))
+        .catch(error=>console.log('error updating file'));;
+        setNotification({status:'Updated Successfully',css:'updated'});
+        setTimeout(()=>setNotification(''),2000);
     }
   
     setformArray({ 'name': '', 'number': '' });
   }
 
   const handleDelete = (id) => {
-    window.confirm("Do you want to delete this ") ?
-    phonebook_ser.clean(id)
+    let d=window.confirm("Do you want to delete this ")
+    if (d) {phonebook_ser.clean(id)
     .then((response) => setPersons(persons.filter(person => person.id !== id)))
-    :console.log("Delete operation cancelled")
+    .catch(error=>console.log('error deleting file'));
+    setNotification({status:'Deleted Successfully',css:'deleted'});
+    setTimeout(()=>setNotification(''),2000);
+  }
+    else {console.log("Delete operation cancelled")}
   }
 
   return (
     <div>
       <form onSubmit={handleChange}>
+        <NotificationMsg noti={notification}/>
         <label>Name : </label>
         <input onChange={(event)=>setformArray({...formArray,'name':event.target.value})} value={formArray.name}/><br/><br/>
         <label>Number : </label>
@@ -65,7 +80,6 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => <Person key={person.id} person={person} handleDeleteOf={()=>handleDelete(person.id)}/> )}
-    
     </div>
   )
 }
